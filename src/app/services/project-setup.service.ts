8603280652805e52f39fc53b8e849358f73282d8
Nikkie <nikkierendler@gmail.com>
2020-02-08 02:13:37 +0200

    logo & project-preview mobile styling

--- src/app/services/project-setup.service.ts
+++ src/app/services/project-setup.service.ts
@@ -20,20 +20,20 @@ export interface StrongSides {
 export class ProjectSetupService {
 
   // TESTING
-  // customerProjectDesign: BehaviorSubject<string> = new BehaviorSubject<string>('flat');
-  // customerProjectTheme: BehaviorSubject<string> = new BehaviorSubject<string>('hsl(321, 90%, 25%)');
-  // customerProjectName: BehaviorSubject<string> = new BehaviorSubject<string>('Project name');
-  // customerProjectExistance: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  // customerProjectTitle: BehaviorSubject<string> = new BehaviorSubject<string>('We are the legion you despise');
-  // customerProjectFeatures: BehaviorSubject<String[]> = new BehaviorSubject<String[]>(['Until the world cames crushing down', 'So let us stand out battleground', 'Everything that I need']);
-  // customerProjectDescription: BehaviorSubject<string> = new BehaviorSubject<string>("Sake expires from child to child in a silver screen portation. It's californication. Dream of californication");
+  customerProjectDesign: BehaviorSubject<string> = new BehaviorSubject<string>('flat');
+  customerProjectTheme: BehaviorSubject<string> = new BehaviorSubject<string>('hsl(321, 90%, 25%)');
+  customerProjectName: BehaviorSubject<string> = new BehaviorSubject<string>('Project name');
+  customerProjectExistance: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  customerProjectTitle: BehaviorSubject<string> = new BehaviorSubject<string>('We are the legion you despise');
+  customerProjectFeatures: BehaviorSubject<String[]> = new BehaviorSubject<String[]>(['Until the world cames crushing down', 'So let us stand out battleground', 'Everything that I need']);
+  customerProjectDescription: BehaviorSubject<string> = new BehaviorSubject<string>("Sake expires from child to child in a silver screen portation. It's californication. Dream of californication");
 
-  customerProjectDesign: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  customerProjectTheme: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  customerProjectName: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  customerProjectExistance: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
-  customerProjectTitle: BehaviorSubject<string> = new BehaviorSubject<string>('');
-  customerProjectFeatures: BehaviorSubject<String[]> = new BehaviorSubject<String[]>(['', '', '']);
-  customerProjectDescription: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  // customerProjectDesign: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  // customerProjectTheme: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  // customerProjectName: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  // customerProjectExistance: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(null);
+  // customerProjectTitle: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  // customerProjectFeatures: BehaviorSubject<String[]> = new BehaviorSubject<String[]>(['', '', '']);
+  // customerProjectDescription: BehaviorSubject<string> = new BehaviorSubject<string>('');
   constructor() { }
 }
